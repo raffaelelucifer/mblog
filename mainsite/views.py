2362ef092cb2322ac5cb60684a2e546ee90eab63
raffaelelucifer <1625504587@qq.com>
2018-01-02 17:52:46 +0800

    P60

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.template.loader import get_template
 from django.http import HttpResponse
 from mainsite.models import Post
+from datetime import datetime
 
 def homepage(request):
+    template = get_template('index.html')
     posts = Post.objects.all()
-    post_lists = list()
-    for count, post in enumerate(posts):
-        post_lists.append("No." +str(count)+':'+str(post)+"<br>")
-        post_lists.append(str(post.body)+"<br><br>")
-    return HttpResponse(post_lists)
+    now = datetime.now()
+    html = template.render(locals())
+    #post_lists = list()
+    #for count, post in enumerate(posts):
+    #    post_lists.append("No." +str(count)+':'+str(post)+"<br>")
+    #    post_lists.append(str(post.body)+"<br><br>")
+    #return HttpResponse(post_lists)
+    return HttpResponse(html)
